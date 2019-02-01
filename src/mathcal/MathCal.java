4ee0e9cbe0b215d491c9129c8a469fc05b29028c
ru2@students.ptcollege.edu <>
2019-02-01 10:11:05 -0500

    First Commit

---
+++ src/mathcal/MathCal.java
@@ -0,0 +1,12 @@
+
+package mathcal;
+
+
+public class MathCal {
+
+    
+    public static void main(String[] args) {
+        System.out.println("The square root of 37 is " + Math.sqrt(37));
+    }
+    
+}
